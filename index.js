4c058ff02e9af2aae223f6d6808e3ef42c3c4f89
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 22:06:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// 19.	Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let guestinvited = [6, 4, 2];
+console.log(` Number of Guests we are inviting are ${guestinvited[0]}`);
+console.log(` Number of Guests we are inviting are ${guestinvited[1]}`);
+console.log(` Number of Guests we are inviting are ${guestinvited[2]}`);
---
+++ index.ts
@@ -0,0 +1,8 @@
+
+// 19.	Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestinvited: number [] = [6,4,2]
+
+console.log(` Number of Guests we are inviting are ${guestinvited[0]}`)
+console.log(` Number of Guests we are inviting are ${guestinvited[1]}`)
+console.log(` Number of Guests we are inviting are ${guestinvited[2]}`)
